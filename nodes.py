32773d161edecd2eb5a6fb5dc6bb3911b189e3aa
Hapseleg <nicolajljo@gmail.com>
2025-05-03 18:44:07 +0200

    new stuff

--- nodes.py
+++ nodes.py
@@ -29,35 +29,45 @@ class MatrixPromptList:
     def INPUT_TYPES(self):
         return {
             "required": {
-                "STRING": ("STRING", {"multiline": True}),
+                "prompt": ("STRING", {"forceInput": True, "multiline": True}),
+                "matrix_prompt": ("STRING", {"multiline": True}),
                 "delimiter": ("STRING", {"default": "|"}),
-                "put_at_start": ("BOOLEAN", {"default": True})
+                "put_at_start": ("BOOLEAN", {"default": True}),
             }
         }
     
     TITLE = "Matrix prompt list"
-    RETURN_TYPES = ("LIST", "STRING", "LIST", "INT", "INT",)
-    RETURN_NAMES = ("all_prompts", "row_prompts", "combination_count", "row_count", "column_count", )
-    OUTPUT_IS_LIST = (True, False, False, False, False,)
+    RETURN_TYPES = ("LIST", "LIST", "STRING", "LIST", "INT", "INT",)
+    RETURN_NAMES = ("base_prompts", "full_prompts", "row_prompts", "combination_count", "row_count", "column_count", )
+    OUTPUT_IS_LIST = (True, True, False, False, False, False,)
     FUNCTION = "run"
     CATEGORY = "utils"
 
-    def run(self, STRING: str, delimiter: str, put_at_start: bool):
+    def run(self, prompt: str, matrix_prompt: str, delimiter: str, put_at_start: bool, ):
         
         # credit goes to automatic1111 for this code
         # https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/scripts/prompt_matrix.py#L73
+        prompt = prompt.strip()
+        matrix_prompt = matrix_prompt.strip()
+
+        if put_at_start == False and prompt[-1] == ',':
+             prompt = prompt[:-1]
         
+        matrix_prompt_concat = prompt + delimiter + matrix_prompt
+
         all_prompts = []
-        prompt_matrix_parts = STRING.split('|')
+        all_base_prompts = []
+        prompt_matrix_parts = matrix_prompt_concat.split(delimiter)
         combination_count = 2 ** (len(prompt_matrix_parts) - 1)
         for combination_num in range(combination_count):
             selected_prompts = [text.strip().strip(',') for n, text in enumerate(prompt_matrix_parts[1:]) if combination_num & (1 << n)]
+            all_base_prompts.append(','.join(selected_prompts))
+
             if put_at_start:
                 selected_prompts = selected_prompts + [prompt_matrix_parts[0]]
             else:
                 selected_prompts = [prompt_matrix_parts[0]] + selected_prompts
-            print(selected_prompts)
-            all_prompts.append(delimiter.join(selected_prompts))
+            all_prompts.append(','.join(selected_prompts))
         
         
         # https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/images.py#L36
@@ -84,12 +94,11 @@ class MatrixPromptList:
                 print(n)
                 print(all_prompts[n])
                 row_prompts += all_prompts[n]
-                if n < column_count + x:
-                    row_prompts += "|"
-                # else:
+                if n < column_count + x - 1:
+                    row_prompts += delimiter
             row_prompts += "\n"
             x += column_count
         print(row_prompts)
 
         
-        return (all_prompts, row_prompts, combination_count, row_count, column_count, )
+        return (all_base_prompts, all_prompts, row_prompts, combination_count, row_count, column_count, )
